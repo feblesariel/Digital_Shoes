2e65398d052625d04b3194211ff6140b65132b49
feblesariel <feblesariel@gmail.com>
2022-04-22 19:06:38 -0300

    V1.0

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log('Server running in 3000 port');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('./views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.resolve('./views/product.html'));
+});
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve('./views/carrito.html'));
+});
