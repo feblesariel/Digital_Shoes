f9b7c4992127e951635ef1e74dc86cc8b7ab9c22
feblesariel <feblesariel@gmail.com>
2022-05-23 19:26:04 -0300

    Actualizacion

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -10,6 +10,11 @@ const { validationResult } = require("express-validator");
 const usersFilePath = path.join(__dirname, "../data/userData.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
+// ************ Archivo de productos ************
+
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
 
 // ************ Controllers ************
 
@@ -17,37 +22,9 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 const mainController = {
 
     home: function (req, res) {
-        const shoes = [
-            {
-                nombre: 'Air Jordan',
-                precio: '$1850.00',
-                imagen: '<img src="./images/zapatillas-Air-Jordan.png" class="product-img" alt="">'
-            },
-            {
-                nombre: 'Air Max',
-                precio: '$1600.00',
-                imagen: '<img src="./images/zapatillas-nike-air-max_ccexpress.png" class="product-img" alt="">'
-            },
-            {
-                nombre: 'Nike Blue',
-                precio: '$2050.00',
-                imagen: '<img src="./images/zapatillas-nike-azul.png" class="product-img" alt="">'
-            },
-            {
-                nombre: 'Flywire',
-                precio: '$2250.00',
-                imagen: '<img src="./images/zapatillas-nike-flywire.png" class="product-img" alt="">'
-            },
-            {
-                nombre: 'Zapatillas',
-                precio: '$2000.00',
-                imagen: '<img src="./images/zapatillas.png" class="product-img" alt="">'
-            }
-
-        ]
-
-        res.render("home", { shoes: shoes });
+        res.render("home", { products: products });
     },
+
     product: function (req, res) {
         res.render("product");
     },
